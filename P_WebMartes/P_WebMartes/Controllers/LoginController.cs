74039c9f55b26916e8a7e5c0e1c42ad7ff6375d3
Brandruiz7 <bruiz20932@ufide.ac.cr>
2024-04-10 22:08:03 -0600

    Proyecto_principal
    - Práctica proyecto progra

---
+++ P_WebMartes/P_WebMartes/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using P_WebMartes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P_WebMartes.Controllers
+{
+    public class LoginController : Controller
+    {
+        UserModel model = new UserModel();
+
+        public ActionResult Login()
+        {
+            return View();
+        } 
+        
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Home() 
+        { 
+            return View();
+        }
+    }
+}
---
+++ P_WebMartes/P_WebMartes/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P_WebMartes.Models
+{
+    public class UserModel
+    {
+    }
+}
