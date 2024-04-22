9b08a7574fc37e9d22cd66b086152ff919bb73e2
Brandruiz7 <bruiz20932@ufide.ac.cr>
2024-04-22 06:34:53 -0600

    Reapply "Semana 5"
    This reverts commit e9bf133f80e923102d495c9b12741c6d36f0a104.

---
+++ P_WebMartes/P_WebMartes/Entidades/RequestMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P_WebMartes.Entidades
+{
+    public class RequestMessage
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
